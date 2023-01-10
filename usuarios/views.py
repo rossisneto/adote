08be78d6503e56c2fe7069fa3b6943910d5d0b44
Rossi Silverio Neto <46680542+rossisneto@users.noreply.github.com>
2023-01-10 04:51:53 -0300

    implementação basica da rota de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 def cadastro(request):
-    return HttpResponse('Olá from Cadastro')
-    # Create your views here.
+    return render(request, 'cadastro.html')
+    #return HttpResponse('Olá from Cadastro')
+    
